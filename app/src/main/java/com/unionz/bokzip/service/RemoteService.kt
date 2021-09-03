efbc5431709ef595ec035f04c95b220cfc60f931
youngjinc <cyjin6789@gmail.com>
2021-09-04 08:54:17 +0900

    [feat:20] 중앙부처 복지 스크랩 DB연동

--- app/src/main/java/com/unionz/bokzip/service/RemoteService.kt
+++ app/src/main/java/com/unionz/bokzip/service/RemoteService.kt
@@ -9,26 +9,30 @@ import com.unionz.bokzip.model.RecommendBokjiItem
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface RemoteService {
 
-    // 추천 탭에서 중앙부처의 특정 분야에 해당하는 복지리스트 가져오기
+    // 추천 탭
     @GET("/post/center/category/{category}")
     fun getRecommendBokji(@Path("category") category: String): Call<ArrayList<RecommendBokjiItem>>
-
-    // 추천 탭에서 중앙부처 복지 클릭 시 복지 상세 정보 가져오기
-    @GET("/post/center/{id}")
-    fun getDetailRecommendBokji(@Path("id") id: String): Call<RecommendBokjiContent>
-
-    // 일반 탭 복지 리스트 가져오기
     @GET("/post/generals")
     fun getGeneralBokji(): Call<ArrayList<GeneralBokjiItem>>
 
-    // 일반 탭 복지 클릭 시 복지 상세정보 가져오기
+    // 상세정보
+    @GET("/post/center/{id}")
+    fun getDetailRecommendBokji(@Path("id") id: String): Call<RecommendBokjiContent>
     @GET("/post/general/{id}")
     fun getDetailGeneralBokji(@Path("id") id: String): Call<GeneralBokjiContent>
+
+    // 스크랩
+    @POST("/post/center/{id}")
+    fun saveCenterScrap(@Path("id") id: String): Call<RecommendBokjiItem>
+    @DELETE("/post/center/{id}")
+    fun removeCenterScrap(@Path("id") id: String): Call<Void>
 
     // static 처럼 공유객체로 사용가능, 모든 인스턴스가 공유하는 객체로서 동작
     companion object {
