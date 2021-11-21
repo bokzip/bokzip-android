3a26e9a597d07c4905d368f9a02860682645ee35
youngjinc <cyjin6789@gmail.com>
2021-11-21 20:52:38 +0900

    [feat:#16] 로그인 및 회원가입 구현

--- app/src/main/java/com/unionz/bokzip/sign/SigninActivity.kt
+++ app/src/main/java/com/unionz/bokzip/sign/SigninActivity.kt
@@ -2,9 +2,7 @@ package com.unionz.bokzip.sign
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.unionz.bokzip.IntroActivity
 import com.unionz.bokzip.R
 import com.unionz.bokzip.home.MainActivity
 import kotlinx.android.synthetic.main.activity_signin.*
@@ -13,22 +11,21 @@ class SigninActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin)
+
+        setListener()
     }
 
-    fun onClick(v : View) {
-        when(v){
-            kakao -> { // @TODO : 카카오, 구글 로그인 구현하기 (현재 : 임시로 카테고리 설정 화면으로 이동)
-                var intent = Intent(this, SignupCategorySetActivity::class.java)
-                startActivity(intent)
-            }
-            google -> {
+    private fun setListener() {
+        kakao.setOnClickListener {
+        }
 
-            }
-            browsing -> { // 로그인 전 둘러보기 버튼
-                var intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-                IntroActivity.prefs.setCategory("지원")
-            }
+        google.setOnClickListener {
+            startActivity(Intent(this, WebViewActivity::class.java))
+        }
+
+        browsing.setOnClickListener {
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
---
+++ app/src/main/java/com/unionz/bokzip/util/Util.kt
@@ -0,0 +1,6 @@
+package com.unionz.bokzip.util
+
+import android.webkit.CookieManager
+
+var cookieManager: CookieManager = CookieManager.getInstance()
+var prefs: PreferenceUtil? = null
