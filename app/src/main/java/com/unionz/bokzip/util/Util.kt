372cf9f8acf744842e806c53662a8f590d9cfb9b
youngjinc <cyjin6789@gmail.com>
2021-12-04 19:01:36 +0900

    [feat] 중복 클릭 방지 클릭 리스너 추가

--- app/src/main/java/com/unionz/bokzip/util/Util.kt
+++ app/src/main/java/com/unionz/bokzip/util/Util.kt
@@ -1,6 +1,14 @@
 package com.unionz.bokzip.util
 
+import android.view.View
 import android.webkit.CookieManager
+import com.unionz.bokzip.OnThrottleClickListener
 
 var cookieManager: CookieManager = CookieManager.getInstance()
 var prefs: PreferenceUtil? = null
+
+/** 중복 클릭 방지 */
+fun View.onThrottleClick(action: (v: View) -> Unit) {
+    val listener = View.OnClickListener { action(it) }
+    setOnClickListener(OnThrottleClickListener(listener))
+}
